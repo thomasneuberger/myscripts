8fdbdf9ec6bec4edc43d9e91c54ace1ceb65fc7c
Thomas Neuberger <thomas.neuberger@sdx-ag.de>
2023-08-09 09:23:34 +0200

    Add AD user info script

---
+++ GetUserInformationFromAd.ps1
@@ -0,0 +1,17 @@
+﻿cls
+[System.Reflection.Assembly]::LoadWithPartialName("System.DirectoryServices.AccountManagement") | Out-Null
+[System.DirectoryServices.AccountManagement.PrincipalContext]$context = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::Domain)
+[System.DirectoryServices.AccountManagement.UserPrincipal]$user = New-Object System.DirectoryServices.AccountManagement.UserPrincipal($context)
+$user.SamAccountName = "thomas"
+#$user.Surname = "Neuberger"
+[System.DirectoryServices.AccountManagement.PrincipalSearcher]$searcher = New-Object System.DirectoryServices.AccountManagement.PrincipalSearcher
+$searcher.QueryFilter = $user
+[System.DirectoryServices.AccountManagement.PrincipalSearchResult[System.DirectoryServices.AccountManagement.Principal]]$result = $searcher.FindAll()
+$userObject = $result | %{ 
+    Write-Host "SID" $_.SID;
+    $_.GetUnderlyingObject() 
+};
+$userObject | Format-List ([string[]]($userObject | Get-Member -MemberType Property | %{ $_.Name } | Sort-Object))
+$searcher.Dispose()
+$user.Dispose()
+$context.Dispose()

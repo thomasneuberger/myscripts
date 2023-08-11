8f8eb8212b8726299f4a59cc4874c367e26fa2d3
Thomas Neuberger <thomas.neuberger@sdx-ag.de>
2023-08-11 13:31:24 +0200

    Parameterize update script for diferent computers

--- update-all.ps1
+++ update-all.ps1
@@ -1,17 +1,33 @@
+[CmdletBinding()]
+param (
+    [switch] $updateBicep,
+    [switch] $updateWsl,
+    [switch] $repairWslDns
+)
 #Requires -RunAsAdministrator
 
 # Update Windows software
 winget upgrade --all
 
 # Update git for windows
-git update-git-for-windows
+# git update-git-for-windows
 
-# Update bicep
-az bicep upgrade
+if ($updateBicep){
+    # Update bicep
+    az bicep upgrade
+}
 
-# Update Ubuntu software in WSL
-$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-$scriptPath = $scriptPath.Replace("C:", "/mnt/c").Replace([System.IO.Path]::DirectorySeparatorChar, "/")
-wsl --exec cp $scriptPath/update-all.sh /home/thomas/update-all.sh
-wsl --exec chmod +X /home/thomas/update-all.sh
-wsl --exec sudo /home/thomas/update-all.sh "$scriptPath"
+if ($updateWsl){
+    # Update Ubuntu software in WSL
+    $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+    $scriptPath = $scriptPath.Replace("C:", "/mnt/c").Replace([System.IO.Path]::DirectorySeparatorChar, "/")
+    wsl --exec cp $scriptPath/update-all.sh /home/thomas/update-all.sh
+    wsl --exec chmod +X /home/thomas/update-all.sh
+    if ($repairWslDns){
+        [string]$repairDnsParameter = "y"
+    }
+    else{
+        [string]$repairDnsParameter = "n"
+    }
+    wsl --exec sudo /home/thomas/update-all.sh "$scriptPath" $repairDnsParameter
+}
--- update-all.sh
+++ update-all.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-cp "$1/resolv.conf" /etc/resolv.conf
+if [ $2 = 'y' ]; then
+    cp "$1/resolv.conf" /etc/resolv.conf
+fi
 apt update
 apt upgrade -y
 apt autoremove -y

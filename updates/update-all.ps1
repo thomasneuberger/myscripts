b5840cf08e3476600743533e4056317061049636
Thomas Neuberger <thomas.neuberger@sdx-ag.de>
2023-10-20 15:10:00 +0200

    Update multiple Ubuntu WSL distributions

--- updates/update-all.ps1
+++ updates/update-all.ps1
@@ -7,27 +7,37 @@ param (
 #Requires -RunAsAdministrator
 
 # Update Windows software
+Write-Host "Update Windows software..." -ForegroundColor Green
 winget upgrade --all --silent
-
-# Update git for windows
-# git update-git-for-windows
 
 if ($updateBicep){
     # Update bicep
+    Write-Host "Update Bicep..." -ForegroundColor Green
     az bicep upgrade
 }
 
 if ($updateWsl){
-    # Update Ubuntu software in WSL
+    Write-Host "Update WSL..." -ForegroundColor Green
+    wsl --update
+
     $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
     $scriptPath = $scriptPath.Replace("C:", "/mnt/c").Replace([System.IO.Path]::DirectorySeparatorChar, "/")
-    wsl --exec cp $scriptPath/update-all.sh /home/thomas/update-all.sh
-    wsl --exec chmod +X /home/thomas/update-all.sh
-    if ($repairWslDns){
-        [string]$repairDnsParameter = "y"
+
+    $ubuntuDistros = wsl --list --all | ForEach-Object { $_.Replace([string][char]0, "").Replace(" (Standard)", "") } | Where-Object { $_ -match "Ubuntu" }
+
+    # Update Ubuntu software in WSL
+    foreach ($distro in $ubuntuDistros) {
+        Write-Host "Update WSL-Distrobution $distro..." -ForegroundColor Green
+
+        wsl --exec cp $scriptPath/update-all.sh /home/thomas/update-all.sh
+        wsl --exec chmod +X /home/thomas/update-all.sh
+        if ($repairWslDns){
+            [string]$repairDnsParameter = "y"
+        }
+        else{
+            [string]$repairDnsParameter = "n"
+        }
+        wsl --exec sudo /home/thomas/update-all.sh "$scriptPath" $repairDnsParameter
     }
-    else{
-        [string]$repairDnsParameter = "n"
-    }
-    wsl --exec sudo /home/thomas/update-all.sh "$scriptPath" $repairDnsParameter
+
 }
